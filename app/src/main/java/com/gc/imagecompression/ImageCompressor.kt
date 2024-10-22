8cf4acb45d9148390f4ebb4286a94aae74b9df86
gc <dev.gauravchauhan@gmail.com>
2024-10-22 09:06:36 +0530

    adding image compression

---
+++ app/src/main/java/com/gc/imagecompression/ImageCompressor.kt
@@ -0,0 +1,27 @@
+package com.gc.imagecompression
+
+import android.content.Context
+import android.net.Uri
+
+
+//
+// Created by Code For Android on 22/10/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+class ImageCompressor(
+    private val context : Context
+) {
+
+    suspend fun compressImage(
+        contentUri : Uri,
+        compressionThreshold : Long,
+    ) : ByteArray? {
+
+
+
+    }
+
+
+
+}
