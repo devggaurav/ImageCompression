c2fc99d6595e1b43d86b2e60cae4488b5cb2ca3a
gc <dev.gauravchauhan@gmail.com>
2024-10-22 09:12:45 +0530

    adding image compression

--- app/src/main/java/com/gc/imagecompression/ImageCompressor.kt
+++ app/src/main/java/com/gc/imagecompression/ImageCompressor.kt
@@ -1,7 +1,12 @@
 package com.gc.imagecompression
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
 //
@@ -18,6 +23,32 @@ class ImageCompressor(
         compressionThreshold : Long,
     ) : ByteArray? {
 
+        return withContext(Dispatchers.IO){
+            val mimeType = context.contentResolver.getType(contentUri)
+             val inputBytes = context.contentResolver.openInputStream(contentUri)
+                 ?.use { it.readBytes() } ?: return@withContext null
+
+            val bitmap = BitmapFactory.decodeByteArray(inputBytes,0,inputBytes.size)
+
+            val comporessFormat = when(mimeType){
+                "image/jpeg" -> Bitmap.CompressFormat.JPEG
+                "image/png" -> Bitmap.CompressFormat.PNG
+                "image/webp" -> if (Build.VERSION.SDK_INT >= 30) {
+                       Bitmap.CompressFormat.WEBP_LOSSLESS
+                }else {
+                    Bitmap.CompressFormat.WEBP
+                }
+            else -> Bitmap.CompressFormat.JPEG
+
+            }
+
+
+            do {
+
+            }while ()
+
+
+        }
 
 
     }
